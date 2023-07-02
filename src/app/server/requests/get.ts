759f10de9f1f5a36edcc6e147c1099b0b6a61a12
IvanMakarishchev <denis.flank@gmail.com>
2023-07-03 00:56:31 +0200

    feat: process PUT requests

--- src/app/server/requests/get.ts
+++ src/app/server/requests/get.ts
@@ -13,21 +13,21 @@ class RequestGet {
       server.on('request', (req, res) => {
         const requestUrl = req.url;
         const requestMethod = req.method;
+        const urlEnd =
+          requestUrl && requestUrl.slice(requestUrl.lastIndexOf('/') + 1);
+        const userId = urlEnd !== 'users' && urlEnd;
+        console.log(userId);
         if (requestMethod === 'GET') {
           const data = this.dataStotage.getData();
-          requestUrl &&
-          requestUrl.slice(requestUrl.lastIndexOf('/') + 1) === 'users'
+          urlEnd === 'users'
             ? res.end(() => {
                 req.statusCode = 200;
                 console.log(`Status code ${req.statusCode}`);
                 console.log(data);
               })
             : res.end(() => {
-                const userId = requestUrl
-                  ? requestUrl.slice(requestUrl.lastIndexOf('/') + 1)
-                  : undefined;
                 const user = data.find((el) => el.id === userId);
-                if (!userId?.match(uuidReg)) {
+                if (!(<string>userId)?.match(uuidReg)) {
                   req.statusCode = 400;
                   console.error(`Status code ${req.statusCode}: Invalid uuid!`);
                 } else if (!user) {
@@ -38,7 +38,7 @@ class RequestGet {
                 } else if (user) {
                   req.statusCode = 200;
                   console.log(`Status code ${req.statusCode}`);
-                  console.log(data.find((el) => el.id === userId));
+                  console.log(user);
                 } else {
                   req.statusCode = 520;
                   console.log(
@@ -69,6 +69,45 @@ class RequestGet {
                 console.log(`Status code ${req.statusCode}`);
                 console.log(this.dataStotage.getData());
               });
+            }
+          });
+        }
+        if (requestMethod === 'PUT') {
+          let body = '';
+          req.on('data', (chunk) => {
+            body += chunk;
+          });
+          res.end(() => {
+            const bodyParsed = JSON.parse(body);
+            if (!isData(bodyParsed)) {
+              res.end(() => {
+                req.statusCode = 400;
+                console.log(
+                  `Status code ${req.statusCode}: Request body does not meet the requirements!`,
+                );
+              });
+            } else {
+              const data = this.dataStotage.getData();
+              const user = data.find((el) => el.id === userId);
+              if (!(<string>userId)?.match(uuidReg)) {
+                req.statusCode = 400;
+                console.error(`Status code ${req.statusCode}: Invalid uuid!`);
+              } else if (!user) {
+                req.statusCode = 404;
+                console.error(
+                  `Status code ${req.statusCode}: User ${userId} not found!`,
+                );
+              } else if (user) {
+                req.statusCode = 200;
+                console.log(`Status code ${req.statusCode}`);
+                this.dataStotage.changeData(<string>userId, bodyParsed);
+                console.log(this.dataStotage.getData());
+              } else {
+                req.statusCode = 520;
+                console.log(
+                  `Status code ${req.statusCode}: Server is returning an unknown error.`,
+                );
+              }
             }
           });
         }
--- src/app/utils/dataService.ts
+++ src/app/utils/dataService.ts
@@ -1,4 +1,4 @@
-import { Data } from "app/interfaces/data";
+import { Data } from 'app/interfaces/data';
 
 export class DataService {
   private dataStorage: Array<Data> = [];
@@ -6,8 +6,11 @@ export class DataService {
   addData(data: Data) {
     this.dataStorage.push(data);
   }
+  changeData(id: string, data: Data) {
+    const index = this.dataStorage.findIndex((user) => user.id === id);
+    this.dataStorage[index] = { ...this.dataStorage[index], ...data };
+  }
   getData() {
     return this.dataStorage;
   }
-  
 }
